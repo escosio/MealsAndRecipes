bee54fa8981c40f3add7f124408b0664e6693521
Scott D'Antuono <scottd26@gmail.com>
2023-05-21 13:00:19 -0400

    Initial Commit

---
+++ MealsAndRecipes/MealsAndRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsAndRecipesApp.swift
+//  MealsAndRecipes
+//
+//  Created by Scott D'Antuono on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MealsAndRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
